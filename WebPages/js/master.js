61c1823b9786c5728c4706ec716bb6fb68f6b1eb
xufeiyun <vanillapeter@gmail.com>
2013-11-21 22:37:09 +0800

    updates partial pages

--- WebPages/js/master.js
+++ WebPages/js/master.js
@@ -1,36 +1,89 @@
 
-/*-------
-
---------*/
 
 $(document).ready(function () {
-    $("#btnRegister").click(function () {
-        register();
-    });
-	
-    $("#btnLogin").click(function () {
-        login();
-    });
+    testPage();
 });
 
+var loadSpecificPageContent = function (element) {
+    var page = QueryString.getQueryStringByName(location, "page");
+    if (page != "") return;
 
-function register()
-{
-	window.location.href = "register.html";
-}
+    var ele = $(element);
 
-function login()
-{
-	window.location.href = "security.html";
-}
+    var title = ele.attr("cbrc-title");
+    var page = ele.attr("cbrc-page");
 
+    // set title
+    document.title = title;
 
-var removeTopNavigator = function ()
-{
-    $("#__divTopNavBarContainer").outerHTML = "";
+    // load page content
+    showPageContent(page);
 };
 
-var removeLeftMenu = function ()
-{
-    $("#__divLeftNavBarContainer").outerHTML = "";
+var showPageContent = function (page) {
+    var removeTopNavigator = function () {
+        $("#__divTopNavBarContainer").html("");
+    };
+    var removeLeftMenu = function () {
+        $("#__divLeftNavigateMenu").html("");
+    };
+    var showCreateAccount = function () {
+        // show link: create an account
+        $("#__buttonRegisterFirmAccount").show();
+    };
+    var hideCreateAccount = function () {
+        // hide link: create an account
+        $("#__buttonRegisterFirmAccount").hide();
+    };
+    var centerPage = function () {
+        var main = $("#__divMiddleContentConainer");
+        var html = ReadFileAPI.getFileContentsSync(page);
+        main.html(html);
+        hideCreateAccount();
+
+        removeTopNavigator();
+        removeLeftMenu();
+    };
+    var showMainContentArea = function (page) {
+        // load sections firstly
+        var main = $("#__divMiddleContentConainer");
+        var html = '<div class="span2" id="__divLeftNavigateMenu"></div>' +
+			   '<div class="span10" id="__divRightMainContent"></div>';
+        main.html(html);
+
+        // load top nav bar
+        var topbar = $("#__divTopNavBarContainer");
+        html = ReadFileAPI.getFileContentsSync("__topnavbar.html");
+        topbar.html(html);
+
+        // load left menu
+        var menu = $("#__divLeftNavigateMenu");
+        html = ReadFileAPI.getFileContentsSync("__leftnavbar.html");
+        menu.html(html);
+
+        // load page content
+        var content = $("#__divRightMainContent");
+        html = ReadFileAPI.getFileContentsSync(page);
+        content.html(html);
+    };
+
+    // load page content
+    if (page == "__login.html") {
+        centerPage();
+        showCreateAccount();
+    } else if (page == "__register.html" || page == "__security.html") {
+        centerPage();
+    } else {
+        showMainContentArea(page);
+    }
+};
+
+
+var testPage = function () {
+    // get page name
+    var page = QueryString.getQueryStringByName(location, "page");
+    if (page == "") return;
+
+    // load page content
+    showPageContent(page);
 };
