840361e8f94674c8bd9014440ae84b2b62d98b48
xufeiyun <vanillapeter@163.com>
2013-11-22 14:39:31 +0800

    update partial sections

--- WebPages/js/common.js
+++ WebPages/js/common.js
@@ -3,56 +3,6 @@
 
 --------*/
 
-var MenuItemSelected = "CurrentMenuItem";
-
-$(document).ready(function () {
-    setTimeout(loadFunction, 1);
-
-    $(window).scroll(function (e) {
-    });
-});
-
-var loadFunction = function () {
-    var banner = $('#__divTopBannerContainer');
-    var navbar = $('#__divTopNavBarContainer');
-    var leftMenu = $('#__divLeftNavigateMenu');
-    //navbar.css("margin-bottom", (banner.height() + 5) + "px");
-    //parent.css("padding-left", "0px");
-    //parent.css("margin-bottom", (banner.height() + 5) + "px");
-
-    // disable click event for [+]
-    $("button[class='row_expand']").click(function (e) {
-        return false;
-    });
-
-    // one item to show/hide details
-    $("#ReturnItem").click(function (e) {
-        $("#ReturnItemDetails").toggle();
-        var btn = $("#btnDisplayDetail")[0];
-        var css = btn.getAttribute("class");
-        if (css.split("expand").length > 1) {
-            btn.removeAttribute("class");
-            btn.setAttribute("class", "row_collapse");
-        }
-        else {
-            btn.removeAttribute("class");
-            btn.setAttribute("class", "row_expand");
-        }
-    });
-
-    // set title
-    $("#titleCreateAccount").click(function (e) {
-        $("#containerCreateAccount").toggle();
-    });
-    var title = $("#titleCreateAccount");
-    title.attr("title", title.html());
-
-    $("#frmCreateUserGroup").click(function () {
-        $("#divCreateUserGroup").show();
-    });
-};
-
-
 var getPageName = function ()
 {
     var index = window.location.href.lastIndexOf('/') + 1;
--- WebPages/js/master.js
+++ WebPages/js/master.js
@@ -1,14 +1,56 @@
 
+var LastActivedItem = null;
 
-$(document).ready(function () {
+$(document).ready(function ()
+{
     testPage();
 });
 
-var loadSpecificPageContent = function (element) {
+var loadFunction = function () {
+    // disable click event for [+]
+    $("button[class='row_expand']").click(function (e) {
+        return false;
+    });
+
+    // one item to show/hide details
+    $("#ReturnItem").click(function (e) {
+        $("#ReturnItemDetails").toggle();
+        var btn = $("#btnDisplayDetail")[0];
+        var css = btn.getAttribute("class");
+        if (css.split("expand").length > 1) {
+            btn.removeAttribute("class");
+            btn.setAttribute("class", "row_collapse");
+        }
+        else {
+            btn.removeAttribute("class");
+            btn.setAttribute("class", "row_expand");
+        }
+    });
+
+    // set title
+    $("#titleCreateAccount").click(function (e) {
+        $("#containerCreateAccount").toggle();
+    });
+    var title = $("#titleCreateAccount");
+    title.attr("title", title.html());
+
+    $("#frmCreateUserGroup").click(function () {
+        $("#divCreateUserGroup").show();
+    });
+};
+
+var loadSpecificPageContent = function (element)
+{
     var page = QueryString.getQueryStringByName(location, "page");
     if (page != "") return;
 
+    if (LastActivedItem != null)
+    {
+        $(LastActivedItem).parent().removeClass("active");
+    }
     var ele = $(element);
+    ele.parent().addClass("active");
+    LastActivedItem = element;
 
     var title = ele.attr("cbrc-title");
     var page = ele.attr("cbrc-page");
@@ -20,22 +62,28 @@ var loadSpecificPageContent = function (element) {
     showPageContent(page);
 };
 
-var showPageContent = function (page) {
-    var removeTopNavigator = function () {
+var showPageContent = function (page)
+{
+    var removeTopNavigator = function ()
+    {
         $("#__divTopNavBarContainer").html("");
     };
-    var removeLeftMenu = function () {
+    var removeLeftMenu = function ()
+    {
         $("#__divLeftNavigateMenu").html("");
     };
-    var showCreateAccount = function () {
+    var showCreateAccount = function ()
+    {
         // show link: create an account
         $("#__buttonRegisterFirmAccount").show();
     };
-    var hideCreateAccount = function () {
+    var hideCreateAccount = function ()
+    {
         // hide link: create an account
         $("#__buttonRegisterFirmAccount").hide();
     };
-    var centerPage = function () {
+    var centerPage = function ()
+    {
         var main = $("#__divMiddleContentConainer");
         var html = ReadFileAPI.getFileContentsSync(page);
         main.html(html);
@@ -44,40 +92,66 @@ var showPageContent = function (page) {
         removeTopNavigator();
         removeLeftMenu();
     };
-    var showMainContentArea = function (page) {
-        // load sections firstly
-        var main = $("#__divMiddleContentConainer");
-        var html = '<div class="span2" id="__divLeftNavigateMenu"></div>' +
-			   '<div class="span10" id="__divRightMainContent"></div>';
-        main.html(html);
-
-        // load top nav bar
-        var topbar = $("#__divTopNavBarContainer");
-        html = ReadFileAPI.getFileContentsSync("__topnavbar.html");
-        topbar.html(html);
-
-        // load left menu
-        var menu = $("#__divLeftNavigateMenu");
-        html = ReadFileAPI.getFileContentsSync("__leftnavbar.html");
-        menu.html(html);
-
-        // load page content
+    var showMainContentArea = function (page)
+    {
         var content = $("#__divRightMainContent");
+        if (content.length == 0)
+        {
+            // load sections firstly
+            var main = $("#__divMiddleContentConainer");
+            var html = '<div class="span2" id="__divLeftNavigateMenu"></div>' +
+			   '<div class="span10" id="__divRightMainContent"></div>';
+            main.html(html);
+
+            // load top nav bar
+            var topbar = $("#__divTopNavBarContainer");
+            html = ReadFileAPI.getFileContentsSync("__topnavbar.html");
+            topbar.html(html);
+
+            // load left menu
+            var menu = $("#__divLeftNavigateMenu");
+            html = ReadFileAPI.getFileContentsSync("__leftnavbar.html");
+            menu.html(html);
+        }
+        // load page content
+        content = $("#__divRightMainContent");
         html = ReadFileAPI.getFileContentsSync(page);
         content.html(html);
     };
 
     // load page content
-    if (page == "__login.html") {
+    if (page == "__login.html")
+    {
         centerPage();
         showCreateAccount();
-    } else if (page == "__register.html" || page == "__security.html") {
+    } else if (page == "__register.html" || page == "__security.html")
+    {
         centerPage();
-    } else {
+    } else
+    {
         showMainContentArea(page);
     }
+
+    loadFunction();
+
+    setTitle();
 };
 
+var setTitle = function ()
+{
+    var titles = $("*[cbrc-title]");
+    titles.each(function (index)
+    {
+        var t = $(titles[index]);
+        var tag = t[0].tagName;
+        var tip = t.attr("cbrc-title");
+        var title = t.attr("title");
+        if (tag.toLowerCase() != "button" && (title == undefined || title == null || title == ""))
+        {
+            t.attr("title", tip);
+        }
+    });
+};
 
 var testPage = function () {
     // get page name
